68fdf3c092ba1b98222c56f627c86fe0bb419b6f
Zhaofeng Miao <522856232@qq.com>
2024-11-23 21:16:42 +0800

    chore: modify eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,7 +35,7 @@ module.exports = {
     // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
     'standard'
-    
+
   ],
 
   plugins: [
@@ -45,7 +45,7 @@ module.exports = {
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
     // required to lint *.vue files
     'vue'
-    
+
   ],
 
   globals: {
@@ -63,7 +63,9 @@ module.exports = {
 
   // add your custom rules here
   rules: {
-    
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow paren-less arrow functions
@@ -83,7 +85,7 @@ module.exports = {
     // The core 'import/named' rules
     // does not work with type definitions
     'import/named': 'off',
-    
+
     'prefer-promise-reject-errors': 'off',
 
     quotes: ['warn', 'single', { avoidEscape: true }],
